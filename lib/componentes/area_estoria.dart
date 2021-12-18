e0a554bdd577e2bb6664311d982be70e6de35f32
Brian Mazini Siervi <brianmazini@gmail.com>
2021-12-18 19:49:33 -0300

    exibindo foto e nome de usuário

--- lib/componentes/area_estoria.dart
+++ lib/componentes/area_estoria.dart
@@ -1,5 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:facebook_interface/componentes/imagem_perfil.dart';
+import 'package:facebook_interface/dados/dados.dart';
 import 'package:facebook_interface/modelos/modelos.dart';
+import 'package:facebook_interface/uteis/paleta_cores.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -18,15 +21,30 @@ class AreaEstoria extends StatelessWidget {
       child: ListView.builder(
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
           scrollDirection: Axis.horizontal,
-          itemCount: estorias.length,
+          itemCount: estorias.length + 1,
           itemBuilder: (context, indice) {
-            Estoria estoria = estorias[indice];
-            return Padding(
-              padding: EdgeInsets.symmetric(horizontal: 4),
-              child: CartaoEstoria(
-                estoria: estoria,
-              ),
-            );
+            if (indice == 0) {
+              Estoria estoriaUsuario = Estoria(
+                usuario: usuarioAtual,
+                urlImagem: usuarioAtual.urlImagem,
+              );
+
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4),
+                child: CartaoEstoria(
+                  estoria: estoriaUsuario,
+                  adicionarEstoria: true,
+                ),
+              );
+            } else {
+              Estoria estoria = estorias[indice - 1];
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 4),
+                child: CartaoEstoria(
+                  estoria: estoria,
+                ),
+              );
+            }
           }),
     );
   }
@@ -34,8 +52,11 @@ class AreaEstoria extends StatelessWidget {
 
 class CartaoEstoria extends StatelessWidget {
   final Estoria estoria;
+  final bool adicionarEstoria;
 
-  CartaoEstoria({Key? key, required this.estoria}) : super(key: key);
+  const CartaoEstoria(
+      {Key? key, required this.estoria, this.adicionarEstoria = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +70,47 @@ class CartaoEstoria extends StatelessWidget {
             width: 110,
             fit: BoxFit.cover,
           ),
-        )
+        ),
+        Container(
+          height: double.infinity,
+          width: 110,
+          decoration: BoxDecoration(
+              gradient: PaletaCores.degradeEstoria,
+              borderRadius: BorderRadius.circular(12)),
+        ),
+        Positioned(
+          top: 8,
+          left: 8,
+          child: adicionarEstoria
+              ? Container(
+                  height: 40,
+                  width: 40,
+                  decoration: const BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: const Icon(Icons.add),
+                    iconSize: 30,
+                    color: PaletaCores.azulFacebook,
+                    onPressed: () {},
+                  ))
+              : ImagemPerfil(
+                  urlImagem: estoria.usuario.urlImagem,
+                  foiVisualizado: estoria.foiVisualizado,
+                ),
+        ),
+        Positioned(
+          bottom: 8,
+          left: 8,
+          right: 8,
+          child: Text(
+            adicionarEstoria ? "Criar Story" : estoria.usuario.nome,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
       ],
     );
   }
--- lib/uteis/paleta_cores.dart
+++ lib/uteis/paleta_cores.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class PaletaCores {
   static const Color azulFacebook = Color(0xff1777f2);
+  static const LinearGradient degradeEstoria = LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: [Colors.transparent, Colors.black54]);
 }
