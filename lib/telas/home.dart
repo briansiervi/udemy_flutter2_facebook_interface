1d30776b8cdbf0cb32ee934e00e572863ac8eece
Brian Mazini Siervi <brianmazini@gmail.com>
2022-01-23 11:32:55 -0300

    45. Página inicial responsiva

--- lib/telas/home.dart
+++ lib/telas/home.dart
@@ -5,6 +5,7 @@ import 'package:facebook_interface/componentes/cartao_postagem.dart';
 import 'package:facebook_interface/dados/dados.dart';
 import 'package:facebook_interface/modelos/modelos.dart';
 import 'package:facebook_interface/uteis/paleta_cores.dart';
+import 'package:facebook_interface/uteis/responsivo.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 
@@ -18,55 +19,131 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            floating: true,
-            centerTitle: false,
-            title: const Text("facebook",
-                style: TextStyle(
-                  color: PaletaCores.azulFacebook,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 28,
-                  letterSpacing: -1.2,
-                )),
-            actions: [
-              BotaoCirculo(
-                icone: Icons.search,
-                iconeTamanho: 30,
-                onPressed: () {},
-              ),
-              BotaoCirculo(
-                icone: LineIcons.facebookMessenger,
-                iconeTamanho: 30,
-                onPressed: () {},
-              ),
-            ],
+    return const Scaffold(
+        body: Responsivo(
+      mobile: HomeMobile(),
+      desktop: HomeDesktop(),
+      //tablet: HomeTablet(),
+    ));
+  }
+}
+
+class HomeMobile extends StatelessWidget {
+  const HomeMobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          backgroundColor: Colors.white,
+          floating: true,
+          centerTitle: false,
+          title: const Text("facebook-M",
+              style: TextStyle(
+                color: PaletaCores.azulFacebook,
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+                letterSpacing: -1.2,
+              )),
+          actions: [
+            BotaoCirculo(
+              icone: Icons.search,
+              iconeTamanho: 30,
+              onPressed: () {},
+            ),
+            BotaoCirculo(
+              icone: LineIcons.facebookMessenger,
+              iconeTamanho: 30,
+              onPressed: () {},
+            ),
+          ],
+        ),
+        SliverToBoxAdapter(
+          child: AreaCriarPostagem(
+            usuario: usuarioAtual,
           ),
-          SliverToBoxAdapter(
-            child: AreaCriarPostagem(
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaEstoria(
               usuario: usuarioAtual,
+              estorias: estorias,
             ),
           ),
-          SliverPadding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
-            sliver: SliverToBoxAdapter(
-              child: AreaEstoria(
-                usuario: usuarioAtual,
-                estorias: estorias,
-              ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, indice) {
+            Postagem postagem = postagens[indice];
+            return CartaoPostagem(postagem: postagem);
+          }, childCount: postagens.length),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeDesktop extends StatelessWidget {
+  const HomeDesktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaEstoria(
+              usuario: usuarioAtual,
+              estorias: estorias,
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate((context, indice) {
-              Postagem postagem = postagens[indice];
-              return CartaoPostagem(postagem: postagem);
-            }, childCount: postagens.length),
+        ),
+        SliverToBoxAdapter(
+          child: AreaCriarPostagem(
+            usuario: usuarioAtual,
           ),
-        ],
-      ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, indice) {
+            Postagem postagem = postagens[indice];
+            return CartaoPostagem(postagem: postagem);
+          }, childCount: postagens.length),
+        ),
+      ],
+    );
+  }
+}
+
+class HomeTablet extends StatelessWidget {
+  const HomeTablet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.fromLTRB(0, 100, 0, 5),
+          sliver: SliverToBoxAdapter(
+            child: AreaEstoria(
+              usuario: usuarioAtual,
+              estorias: estorias,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: AreaCriarPostagem(
+            usuario: usuarioAtual,
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, indice) {
+            Postagem postagem = postagens[indice];
+            return CartaoPostagem(postagem: postagem);
+          }, childCount: postagens.length),
+        ),
+      ],
     );
   }
 }
--- lib/uteis/responsivo.dart
+++ lib/uteis/responsivo.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Responsivo extends StatelessWidget {
-  const Responsivo({Key? key}) : super(key: key);
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget desktop;
+
+  const Responsivo(
+      {Key? key, required this.mobile, required this.desktop, this.tablet})
+      : super(key: key);
 
   static bool isMobile(BuildContext context) {
     return MediaQuery.of(context).size.width < 800;
@@ -18,6 +24,18 @@ class Responsivo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth >= 1200) {
+        return desktop;
+      } else if (constraints.maxWidth >= 800) {
+        Widget? resultadoTablet = tablet;
+        if (resultadoTablet != null) {
+          return resultadoTablet;
+        }
+        return mobile;
+      } else {
+        return mobile;
+      }
+    });
   }
 }
